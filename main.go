754972e837659d7cb27dcd997407a799da60ab85
eityans <eityans@gmail.com>
2022-04-12 23:57:30 +0900

    revert

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
     router := gin.Default()
     router.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
-            "message": "ここをキャンプ地とする",
+            "message": "pong",
         })
     })
     router.Run()
