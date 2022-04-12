37027fe08655dfa6a5ed913cea5db86312008441
eityans <eityans@gmail.com>
2022-04-13 00:01:05 +0900

    test

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
     router := gin.Default()
     router.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
-            "message": "pong",
+            "message": "poooong",
         })
     })
     router.Run()
