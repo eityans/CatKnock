f6b39d3162c44e4efd6031f5d8e3d5aed00a1146
eityans <eityans@gmail.com>
2022-04-16 01:17:00 +0900

    copy

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
-	"github.com/k0kubun/pp/v3"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 func pingHandler(c *gin.Context) {
@@ -12,10 +16,41 @@ func pingHandler(c *gin.Context) {
 }
 
 func callbackHandler(c *gin.Context) {
-	pp.Print(c.Request)
-	c.JSON(200, gin.H{
-			"message": "aaa",
-	})
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_ACCESS_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	events, err := bot.ParseRequest(c.Request)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			c.Writer.WriteHeader(400)
+		} else {
+			c.Writer.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
+				}
+			case *linebot.StickerMessage:
+				replyMessage := fmt.Sprintf(
+					"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
+
 }
 
 func main() {
