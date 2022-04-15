65293bfc32e492bf9ca90f6026444392a2c7d4bf
eityans <eityans@gmail.com>
2022-04-16 00:19:12 +0900

    divice handler

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func pingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+			"message": "ここをキャンプ地とする",
+	})
+}
+
+func callbackHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+			"message": "aaa",
+	})
+}
 
 func main() {
-    router := gin.Default()
-    router.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "ここをキャンプ地とする",
-        })
-    })
-    router.Run()
+	router := gin.Default()
+	router.GET("/ping", pingHandler)
+	router.GET("/callback", callbackHandler)
+	router.Run()
 }
